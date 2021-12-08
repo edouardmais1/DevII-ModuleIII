d69b19f2354d8c50cc1893b0695f74ae35fe7d35
edouardmais1 <e.maisin@students.ephec.be>
2021-12-09 00:28:14 +0100

    MAJ

--- Functions/Mongo.py
+++ Functions/Mongo.py
@@ -1,13 +1,15 @@
 from pymongo import MongoClient
+
 from src.config import config
+
 
 class MongoConnector:
     """
-    ---> cette classe permet de creer une connexion avec la base de données.
+    ---> permet de se connecter à une base de donnée mongoDb
     """
 
     def __init__(self):
-        certificat_path = config.ROOT_DIR + "C:/Users/User/Desktop/coursBac2/DevBac2/certificatMongo/2TL2-G1.pem"
+        certificat_path = config.ROOT_DIR + "/certif_mongo.pem"
         uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
         client = MongoClient(uri,
                              tls=True,
