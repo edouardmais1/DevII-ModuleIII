2850b1bd62fd58a9736f1400127e3b368ed25414
edouard <edouard.mais1@gmail.com>
2021-12-09 15:04:57 +0100

    MAJ

--- Functions/class_main.py
+++ Functions/class_main.py
@@ -1,5 +1,4 @@
 from Functions.gestion_files import *
-from project_class.User import *
 from project_class.Email import *
 from project_class.Password import *
 
@@ -8,23 +7,15 @@ def inscription():
     '''
     ---> gère l'inscription de l'utilisateur
     ---> pour l'instant, les utilisateur sont stockés dans un fichier texte car MongoDB n'est pas implémenté
+
+    --->realiser une interface inscription/connexion lors du lancement du main
+
+    ---> premiere etape kivy : nom et prénom
+
+    ---> ensuite, email
+    ---> et pour finir password
+
+
+    ---> https://riptutorial.com/Download/kivy-fr.pdf (pdf kivy)
     '''
-    new_email = Email()
-    new_email.setUserName()
-    email = new_email.setEmail()
 
-    if not check_email_exist(path, 'Data', file="DataBase", email=email):
-        new_password = Password()
-        new_password.setPassword()
-        print(new_email.getEmail())
-        print(str(new_password.getPassword()))
-
-        utilisateur = User(new_email.getEmail(), new_password.getPassword())
-        write_file(path, 'Data', file="DataBase", email=utilisateur.getEmail(),
-                   password=utilisateur.getPassword())
-
-        print("Votre inscription a bien été enregistrée !\n")
-
-
-def connexion():
-    pass
