bbe72a96ee01e2f97ec296709c1f3af659fe14ca
M-Momin <m.momin@students.ephec.be>
2021-12-10 16:26:27 +0100

    Kivy en .kv

--- interfaceBuilding/mainInterface.py
+++ interfaceBuilding/mainInterface.py
@@ -1,55 +1,19 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+#type kyprops anytime you need to declare ObjectProperty
+class MyGridLayout(Widget):
 
-class MyGridLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGridLayout, self).__init__(**kwargs)
+    def press_login(self):
+        print("Login pressed")
 
-        # set the columns
-        self.cols = 1
-
-        # cree une seconde boite
-        self.top_grid = GridLayout()
-        self.top_grid.cols = 2
-
-        # add widgets
-        self.top_grid.add_widget(Label(text="email: "))
-
-        # add input boxes
-        self.name = TextInput(multiline=False)
-        self.top_grid.add_widget(self.name)
-
-        # add widgets
-        self.top_grid.add_widget(Label(text="password: "))
-
-        # add input boxes
-        self.pswd = TextInput(multiline=False)
-        self.top_grid.add_widget(self.pswd)
-
-        # rajouter cette boite dans l'interface
-        self.add_widget(self.top_grid)
-
-        # create a submit button
-        self.submit = Button(text="Submit", font_size=32)
-
-        # bind the button
-        self.submit.bind(on_press=self.press)
-        self.add_widget(self.submit)
-
-    def press(self, elem):
-        name = self.name.text
-        pswd = self.pswd.text
-
-        self.add_widget(Label(text="hello {}, your password is {}".format(name, pswd)))
-
-        self.name.text = ""
-        self.pswd.text = ""
-
+    def press_signin(self):
+        print("Signin pressed")
 
 class MainInterface(App):
     def build(self):
