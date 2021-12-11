990f97c8c9ee7b64a24108ea22d7c35f8361c0de
edouard <edouard.mais1@gmail.com>
2021-12-11 15:52:48 +0100

    Kivy MainInterface Maj

--- interfaceBuilding/mainInterface.py
+++ interfaceBuilding/mainInterface.py
@@ -4,16 +4,48 @@ from kivy.config import Config
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
 
 # Connexion and Inscription screen creation
 class ConnexionScreen(Screen):
+    """
+    ---> assure la gestion des inscriptions au sein de l'application
+    """
     pass
 
 
 class InscriptionScreen(Screen):
-    pass
+    """
+    ---> assure la gestion des inscriptions au sein de l'application
+    """
+    prenom_input = ObjectProperty
+
+    def __init__(self, **kwargs):
+        super(InscriptionScreen, self).__init__(**kwargs)
+
+    def validPrenom(self, instance, value):
+        """
+        ---> permet de vérifier la validité du prénom
+        """
+        if any(elem.isdigit() for elem in value):
+            instance.foreground_color = (1, 0, 0, 1)
+
+        elif set('[~!@#$%^&*()_+{}":;\']+$').intersection(value):
+            instance.foreground_color = (1, 0, 0, 1)
+
+        else:
+            instance.foreground_color = (0, 255, 0, 1)
+
+    def getNom(self):
+        pass
+
+    def getEmail(self):
+        pass
+
+    def get_password(self):
+        pass
 
 
 class WindowManager(ScreenManager):
@@ -28,4 +60,5 @@ class MainApp(App):
         return kv
 
 
-MainApp().run()
+if __name__ == '__main__':
+    MainApp().run()
