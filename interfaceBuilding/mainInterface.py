678dad52df03d10c8ae59e9e1047288ab68b7914
edouard <edouard.mais1@gmail.com>
2021-12-11 12:40:06 +0100

    Kivy MainInterface Maj

--- interfaceBuilding/mainInterface.py
+++ interfaceBuilding/mainInterface.py
@@ -1,67 +1,31 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-from test import *
+from kivy.uix.widget import Widget
 
 
-class MyGridLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGridLayout, self).__init__(**kwargs)
-
-        # set the columns
-        self.cols = 1
-
-        # cree une seconde boite
-        self.top_grid = GridLayout()
-        self.top_grid.cols = 2
-
-        # add widgets
-        self.top_grid.add_widget(Label(text="email: "))
-
-        # add input boxes
-        self.name = TextInput(multiline=False)
-        self.top_grid.add_widget(self.name)
-
-        # add widgets
-        self.top_grid.add_widget(Label(text="password: "))
-
-        # add input boxes
-        self.pswd = TextInput(multiline=False)
-        self.top_grid.add_widget(self.pswd)
-
-        # rajouter cette boite dans l'interface
-        self.add_widget(self.top_grid)
-
-        # create a submit button
-        self.login = Button(text="Submit",
-                            font_size=32,
-                            size_hint_y=None,
-                            height=50,
-                            size_hint_x=None,
-                            width=200)
-
-        # bind the button
-        self.login.bind(on_press=self.press_login)
-        self.add_widget(self.login)
-
-    def press_login(self, elem):
-        name = self.name.text
-        pswd = self.pswd.text
-
-        self.add_widget(Label(text="hello {}, your password is {}".format(name, pswd)))
-
-        self.name.text = ""
-        self.pswd.text = ""
+# Connexion and Inscription screen creation
+class ConnexionScreen(Screen):
+    pass
 
 
-class MainInterface(App):
+class InscriptionScreen(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file('Main.kv')
+
+
+class MainApp(App):
     def build(self):
-        return MyGridLayout()
+        return kv
 
 
-if __name__ == '__main__':
-    MainInterface().run()
+MainApp().run()
