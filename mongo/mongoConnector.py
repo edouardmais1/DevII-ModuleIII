f3507692ec5440f4f649cc48d610fd027988ca82
edouard <edouard.mais1@gmail.com>
2021-12-15 20:54:14 +0100

    Kivy MainInterface Maj

--- mongo/mongoConnector.py
+++ mongo/mongoConnector.py
@@ -17,7 +17,8 @@ class MongoConnector:
 
     def __init__(self):
         certificat_path = r"C:\Users\YiddishGun\Desktop\DEV2\certifMongoDB\2TL2-G1.pem"
-        uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom-2TL2?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
+        uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom-2TL2?authSource=%24external&authMechanism=MONGODB" \
+              "-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE "
         client = MongoClient(uri,
                              tls=True,
                              tlsCertificateKeyFile=certificat_path)
