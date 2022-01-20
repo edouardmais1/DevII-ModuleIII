ca6c3e282d99b4b4a63b072350b0f8cac26b9295
Greg Falla <he201939@students.ephec.be>
2022-01-20 16:00:00 +0100

    MAJ Permissions

--- Project_functions/Permissions.py
+++ Project_functions/Permissions.py
@@ -4,3 +4,20 @@
 - [lire, écrire, "partage de fichier", ajouter membre, modifier role de qq, supprimer qq, modification de channel]
 - Perm: def owner_server(), def can_read(), def can_write(), def can_join_file(), def can_add(), def can_change_role(), def can_ban(), def can_change_channel()
 """
+
+def owner_server(user, server):
+
+def can_read(user, server):
+
+def can_write(user, server):
+
+def can_join_file(user, server):
+
+def can_add(user, server):
+
+def can_change_role(user, server):
+
+def can_ban(user, server):
+
+def can_change_channel(user, server):
+
