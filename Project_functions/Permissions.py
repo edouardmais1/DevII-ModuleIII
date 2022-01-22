58add326a26fecc9192c9128e39e11c35582d72a
Greg Falla <he201939@students.ephec.be>
2022-01-22 12:49:52 +0100

    MAJ Permissions

--- Project_functions/Permissions.py
+++ Project_functions/Permissions.py
@@ -1,23 +1,50 @@
+from mongo.mongoConnector import *
 """
-- 0_Stranger,1_membre, 2_coAdmin, 3_Admin --> [1,0,0,0,0,0,0], [1,1,1,0,0,0,0], [1,1,1,1,1,0,0], [1,1,1,1,1,1,1]
+- 0_Guest,1_Member, 2_Admin --> [1,0,0,0,0,0,0], [1,1,1,0,0,0,0], [1,1,1,1,1,1,1]
 - Crée un serveur, perm de ce serveur ADMIN
 - [lire, écrire, "partage de fichier", ajouter membre, modifier role de qq, supprimer qq, modification de channel]
 - Perm: def owner_server(), def can_read(), def can_write(), def can_join_file(), def can_add(), def can_change_role(), def can_ban(), def can_change_channel()
 """
 
-def owner_server(user, server):
+def on_join(userID, serverID):
+    try:
+        with MongoConnector() as connector:
+            collection = connector.db["role_user"]
+            collection.insert_one({"server": serverID, "user": userID, "role": 0})
+    except Exception as e:
+        print(e)
 
-def can_read(user, server):
+def owner_server(userID, serverID):
+    try:
+        with MongoConnector() as connector:
+            collection = connector.db["role_user"]
+            collection.insert_one({"server": serverID, "user": userID, "role": 2})
+    except Exception as e:
+        print(e)
 
-def can_write(user, server):
+def can_read(userID, serverID):
 
-def can_join_file(user, server):
 
-def can_add(user, server):
+def can_write(userID, serverID):
 
-def can_change_role(user, server):
 
-def can_ban(user, server):
+def can_join_file(userID, serverID):
 
-def can_change_channel(user, server):
+
+def can_add(userID, serverID):
+
+
+def can_change_role(userID, serverID):
+
+
+def can_ban(userID, serverID):
+
+
+def can_change_channel(userID, serverID):
+
+def change_role(userID, serverID):
+    if can_change_role(userID, serverID):
+        // change le role
+    else:
+        // Erreur
 
