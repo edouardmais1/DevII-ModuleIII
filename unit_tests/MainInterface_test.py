164dcc7f5f726c1c6e966e7901dd090df87404eb
edouard <edouard.mais1@gmail.com>
2022-01-23 12:59:33 +0100

    maj tests unitaires

--- unit_tests/MainInterface_test.py
+++ unit_tests/MainInterface_test.py
@@ -5,36 +5,38 @@ from Project_functions.Password import *
 from mongo.mongoConnector import *
 
 
-
 class test_checkValidPrenomNom(unittest.TestCase):
     def test_checkValidPrenomNom(self):
         self.assertEqual(checkValidPrenomNom("Maxime"), True, "Maxime est valide")
         self.assertEqual(checkValidPrenomNom("Edouard"), True, "Edouard est valide")
         self.assertEqual(checkValidPrenomNom("Max5"), False, "Présence d'un chiffre, non valide")
-        self.assertEqual(checkValidPrenomNom("E"), False, "False")
-        self.assertEqual(checkValidPrenomNom("zdq*"), False, "False")
+        self.assertEqual(checkValidPrenomNom("E"), False, "prenom ou nom trop court")
+        self.assertEqual(checkValidPrenomNom("zdq*"), False, "présence d'un caractère spécial")
 
 
 class test_checkEmailValidation(unittest.TestCase):
     def test_checkEmailValidation(self):
-        self.assertEqual(checkEmailValidation("maxime", "m.momin@students.ephec.be"), True, "True")
-        self.assertEqual(checkEmailValidation("edouard", "m.momin@students.ephec.be"), False, "False")
-        self.assertEqual(checkEmailValidation("maxime", "m.momin@students.be"), False, "False")
+        self.assertEqual(checkEmailValidation("maxime", "m.momin@students.ephec.be"), True, "l'adresse est valide et "
+                                                                                            "correspond au prénom")
+        self.assertEqual(checkEmailValidation("edouard", "m.momin@students.ephec.be"), False, "le prenom et l'adresse "
+                                                                                              "ne correspondent pas")
+        self.assertEqual(checkEmailValidation("maxime", "m.momin@students.be"), False, "le format de l'adresse saisie "
+                                                                                       "est invalide")
 
 
 class test_checkPassword(unittest.TestCase):
     def test_checkPassword(self):
-        self.assertEqual(checkPassword("Azerty1234*"), True, "True")
-        self.assertEqual(checkPassword("Azerty"), False, "False")
-        self.assertEqual(checkPassword("1234"), False, "False")
-        self.assertEqual(checkPassword("123*azerty"), False, "False")
+        self.assertEqual(checkPassword("Azerty1234*"), True, "le mot de passe est valide")
+        self.assertEqual(checkPassword("Azerty"), False, "le mot de passe ne contient pas de caractère spécial")
+        self.assertEqual(checkPassword("1234"), False, "le mot de passe n'est pas valide (taille, caractères)")
+        self.assertEqual(checkPassword("123*azerty"), False, "le mot de passe ne contient pas de majuscule")
 
 
 class test_checkAccount(unittest.TestCase):
     def test_checkAccount(self):
-        self.assertEqual(checkAccount("m.momin@students.ephec.be"), True, "True")
-        self.assertEqual(checkAccount("Edouard"), False, "False")
-        self.assertEqual(checkAccount("Edouard@students.ephec"), False, "False")
+        self.assertEqual(checkAccount("m.momin@students.ephec.be"), True, "l'email éxiste dans la db")
+        self.assertEqual(checkAccount("Edouard"), False, "l'utilisateur n'éxiste pas dans la db")
+        self.assertEqual(checkAccount("Edouard@students.ephec"), False, "l'utilisateur n'éxiste pas dans la db")
 
     def test_raises_connexion(self):
         self.assertRaises(Exception, checkAccount(""), "Exception")
@@ -42,14 +44,24 @@ class test_checkAccount(unittest.TestCase):
 
 class test_connexion(unittest.TestCase):
     def test_connexion(self):
-        self.assertEqual(connexion("m.momin@students.ephec.be", hashPassword("Azerty123*")), True, "True")
-        self.assertEqual(connexion("lou@students.ephec.be", hashPassword("Azerty123*")), False, "False")
-        self.assertEqual(connexion("lou@ephec.be", hashPassword("Azerty123*")), False, "False")
+        self.assertEqual(connexion("m.momin@students.ephec.be", hashPassword("Azerty123*")), True, "le compte et le "
+                                                                                                   "mot de passe "
+                                                                                                   "associé sont "
+                                                                                                   "valides et "
+                                                                                                   "éxistent dans la "
+                                                                                                   "db")
+
+        self.assertEqual(connexion("lou@students.ephec.be", hashPassword("Azerty123*")), False, "le compte et le mot "
+                                                                                                "de passe associé "
+                                                                                                "n'éxistent pas dans "
+                                                                                                "la db")
+
+        self.assertEqual(connexion("lou@ephec.be", hashPassword("Azerty123*")), False, "le compte et le mot de passe "
+                                                                                       "associé n'existent pas dans "
+                                                                                       "la db")
 
     def test_raises_connexion(self):
         self.assertRaises(Exception, connexion("", hashPassword("")), "Exception")
-
-
 
 
 if __name__ == '__main__':
